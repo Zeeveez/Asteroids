7736513ee57fe44b549db61d528f441507ff86b6
Nicholas <nicholasjalderton@gmail.com>
2021-06-16 22:10:31 +0100

    enable debug

--- Asteroids/Asteroids.cpp
+++ Asteroids/Asteroids.cpp
@@ -30,9 +30,10 @@ MessageCallback(GLenum source,
     const GLchar* message,
     const void* userParam)
 {
-    //fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-    //    (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
-    //    type, severity, message);
+    if (type == 0x8251) { return; }
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+        (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+        type, severity, message);
 }
 
 // During init, enable debug output
